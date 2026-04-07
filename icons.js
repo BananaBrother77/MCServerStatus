419c99ecf077d3ee5e2a5d4159c7c672a2257850
BananaBrother77 <jonas.goltz2011@gmail.com>
2026-04-07 20:13:34 +0200

    added icons

---
+++ icons.js
@@ -0,0 +1,26 @@
+import {
+  createIcons,
+  Server,
+  Users,
+  Info,
+  MessageSquare,
+  Activity,
+  Clock,
+  Cpu,
+  MemoryStick,
+  HardDrive,
+} from 'lucide';
+
+createIcons({
+  icons: {
+    Server,
+    Users,
+    Info,
+    MessageSquare,
+    Activity,
+    Clock,
+    Cpu,
+    MemoryStick,
+    HardDrive,
+  },
+});
